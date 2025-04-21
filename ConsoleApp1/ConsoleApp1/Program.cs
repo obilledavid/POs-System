77d749985e13706843dc292c408a4c161a344320
ongki_cat <134025708+obilledavid@users.noreply.github.com>
2025-04-21 16:41:42 +0800

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace HelloWorld
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int i = 0;
+            do
+            {
+                Console.WriteLine(i);
+                i--;
+
+            }
+            while (i>-5);
+        }
+    }
+}
